3f82bdaff753024276872dc9fd8094f2e73fdc48
shubham changan <98081243+shubhamchangan@users.noreply.github.com>
2022-01-31 16:12:59 +0530

    Add files via upload
    basic things about exception handling.

---
+++ exception_basic.py
@@ -0,0 +1,12 @@
+# compulsory part
+try:
+    
+    a = 445 
+except:
+    print('Error')
+
+# optional part else and finally
+else:
+    print('No error')
+finally:
+    print('Execute in any condition')
